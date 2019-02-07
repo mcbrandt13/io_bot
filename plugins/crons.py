ff1252adaa668fc2b8d29fb28b454eabcb12cd04
mcbrandt13 <mcbrandt13@gmail.com>
2019-02-07 22:19:04 +0000

    crons.py updates

--- plugins/crons.py
+++ plugins/crons.py
@@ -13,27 +13,27 @@ class crons(WillPlugin):
 
     #people = {'Anne': 'EmmaDupin', 'EmmaDupin': 'AngelaMelkisethian', 'AngelaMelkisethian': 'Anne'}
     people = {'Anne':'AngelaMelkisethian', 'AngelaMelkisethian':'Anne'}
-    with codecs.open('/home/kbrandt/bot/plugins/yesterday.txt', 'r') as f:
+    with codecs.open('/home/kbrandt/iobot/io_bot/plugins/yesterday.txt', 'r') as f:
       yesterday = f.readlines()
       yesterday = yesterday[0].strip()
       today = people[yesterday]
-    with codecs.open('/home/kbrandt/bot/plugins/yesterday.txt', 'w') as f:
+    with codecs.open('/home/kbrandt/iobot/io_bot/plugins/yesterday.txt', 'w') as f:
       f.write(today)
 
-    self.say("@{0}, today is your turn to triage support tickets!".format(today), room=self.available_rooms['Publishing Systems Support'])
+    self.say("@{0}, today is your turn to triage support tickets!".format(today), room=self.available_rooms['Editorial Manager Support'])
  
   @periodic(hour='1', minute='00', day_of_week='mon')
   def assess_triage(self):
     review = {'lilcasserole': 'CanadianBacon', 'CanadianBacon': 'ChristopherWiles', 'ChristopherWiles': 'lilcasserole'}
     chasing = {'CanadianBacon': 'lilcasserole', 'lilcasserole': 'CanadianBacon'}
     #ChristopherWiles,lilcasserole
-    with codecs.open('/home/kbrandt/bot/plugins/assess.txt', 'r') as f:
+    with codecs.open('/home/kbrandt/iobot/io_bot/plugins/assess.txt', 'r') as f:
       previous = f.read().split(',')
       previous_review = previous[0].strip()
       previous_chase = previous[1].strip()
       current_review = review[previous_review]
       current_chase = chasing[previous_chase]
-    with codecs.open('/home/kbrandt/bot/plugins/assess.txt', 'w') as f:
+    with codecs.open('/home/kbrandt/iobot/io_bot/plugins/assess.txt', 'w') as f:
       f.write(','.join([current_review, current_chase]))
 
     self.say("@{0}, this week is your turn on the Revise report! @{1}, this week is your turn on the Chasing queue!".format(current_review, current_chase), room=self.available_rooms['GEP'])
